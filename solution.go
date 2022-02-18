9bb774eddd606a353659cb1af721b0dc2f9d6eb6
UlyanaDerkach <derkach.1103@gmail.com>
2022-02-18 23:47:24 +0600

    task 2

--- solution.go
+++ solution.go
@@ -17,7 +17,7 @@ const SidesCircle = 0
 
 func CalcSquare(sideLen float64, sidesNum sideType) float64 {
 	if sidesNum == SidesTriangle {
-		return math.Sqrt(3) * sideLen / 4
+		return math.Sqrt(3) * math.Pow(sideLen, 2) / 4
 	} else if sidesNum == SidesSquare {
 		return math.Pow(sideLen, 2)
 	} else if sidesNum == SidesCircle {
